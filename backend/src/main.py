72d0e410ac658fff43278e43f399e60699d1789a
solano_b <solano_b@etna-alternance.net>
2023-06-18 22:39:55 +0200

    deps: changed firestore-key to gpartner account

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,10 +1,10 @@
 # import uvicorn
 from fastapi import FastAPI
-from users.router import router as users_router
+from snippets.router import router as snippets_router
 
 app = FastAPI()
 
-app.include_router(users_router)
+app.include_router(snippets_router)
 
 # if __name__ == "__main__":
 # #     uvicorn.run(app, host="0.0.0.0", port=8000)
