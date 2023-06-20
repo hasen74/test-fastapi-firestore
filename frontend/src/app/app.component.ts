7ab8b489084d198fbbdce88b7f5cef137bfb8ca7
solano_b <solano_b@etna-alternance.net>
2023-06-20 22:57:56 +0200

    feat: angular signin with google test

---
+++ frontend/src/app/app.component.ts
@@ -0,0 +1,23 @@
+import { SocialAuthService, SocialUser } from '@abacritt/angularx-social-login';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit{
+
+  user!: SocialUser;
+  loggedIn!: boolean;
+  
+  constructor(private authService: SocialAuthService) { }
+
+  ngOnInit() {
+    this.authService.authState.subscribe((user) => {
+      this.user = user;
+      this.loggedIn = (user != null);
+      console.log(this.user)
+    });
+  }
+}
---
+++ frontend/src/app/app.module.ts
@@ -0,0 +1,37 @@
+import { GoogleLoginProvider, GoogleSigninButtonModule, SocialAuthServiceConfig, SocialLoginModule } from '@abacritt/angularx-social-login';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    SocialLoginModule,
+    GoogleSigninButtonModule
+  ],
+  providers: [
+    {
+      provide: 'SocialAuthServiceConfig',
+      useValue: {
+        autoLogin: false,
+        providers: [
+          {
+            id: GoogleLoginProvider.PROVIDER_ID,
+            provider: new GoogleLoginProvider(
+              '216068480773-7fka82gqqir77gq3f7ih4b3v5n006si8.apps.googleusercontent.com'
+            )
+          }
+        ],
+        onError: (err) => {
+          console.error(err);
+        }
+      } as SocialAuthServiceConfig,
+    }
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
