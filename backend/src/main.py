f88b6086dd3953812c02e455056cb7b4b70dfe2b
hasen74 <solano_b@etna-alternance.net>
2023-06-22 13:55:26 +0200

    feat: added CORS for all origins

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,5 +1,6 @@
 # import uvicorn
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from snippets.router import router as snippets_router
 from users.router import router as users_router
 from languages.router import router as languages_router
@@ -8,6 +9,14 @@ from comments.router import router as comments_router
 from notifications.router import router as notifications_router
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(snippets_router)
 app.include_router(users_router)
